a54d40e93d1a7fedbd3ffe205a65ee848aaf5d6c
trluwc <trinhvanluc412@gmail.com>
2026-06-03 17:13:30 +0700

    fix

--- src/java/dal/UserDAO.java
+++ src/java/dal/UserDAO.java
@@ -8,12 +8,15 @@ import java.sql.*;
 public class UserDAO extends DBContext {
 
     /*
- * Name: login
- * @Author: LUCTVHE201874
- * Date: [01/06/2026]
- * Version: 1.0
- * Description: Hàm này truy vấn vào cơ sở dữ liệu đẻ tìm người dùng theo email, so sánh mật khẩu
-    với mật khẩu đã dc mã hóa(hash) trong database và trả về Users nếu khớp và null nếu sai
+     * Name: login
+     * 
+     * @Author: LUCTVHE201874
+     * Date: [01/06/2026]
+     * Version: 1.0
+     * Description: Hàm này truy vấn vào cơ sở dữ liệu đẻ tìm người dùng theo email,
+     * so sánh mật khẩu
+     * với mật khẩu đã dc mã hóa(hash) trong database và trả về Users nếu khớp và
+     * null nếu sai
      */
     public Users login(String email, String password) {
         String sql = "SELECT * FROM [User] WHERE email = ?";
@@ -42,11 +45,12 @@ public class UserDAO extends DBContext {
     }
 
     /*
- * Name: isEmailExist
- * @Author: LUCTVHE201874
- * Date: [01/06/2026]
- * Version: 1.0
- * Description: check trùng email
+     * Name: isEmailExist
+     * 
+     * @Author: LUCTVHE201874
+     * Date: [01/06/2026]
+     * Version: 1.0
+     * Description: check trùng email
      */
     public boolean isEmailExist(String email) {
         String sql = "SELECT * FROM [User] WHERE email = ?";
@@ -62,11 +66,13 @@ public class UserDAO extends DBContext {
     }
 
     /*
- * Name: register
- * @Author: LUCTVHE201874
- * Date: [01/06/2026]
- * Version: 1.0
- * Description: Hàm này nhận thông tin người dùng , mã hóa mật khảu và thêm bản ghi mới vào cơ sở dữ liệu
+     * Name: register
+     * 
+     * @Author: LUCTVHE201874
+     * Date: [01/06/2026]
+     * Version: 1.0
+     * Description: Hàm này nhận thông tin người dùng , mã hóa mật khảu và thêm bản
+     * ghi mới vào cơ sở dữ liệu
      */
     public boolean register(String userName, String email, String phone, String password) {
         String sql = "INSERT INTO [User] (full_name, email, phone, password, status, role_id) VALUES (?, ?, ?, ?, 'active', 2)";
