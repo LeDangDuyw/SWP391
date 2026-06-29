ce5ae3bb4e985b91d9ea23c4565115cfe25cacaa
minh0407 <quangming0407@gmail.com>
2026-06-29 22:49:51 +0700

    update lai do bug mat code tren github

--- src/java/dal/PromotionsDAO.java
+++ src/java/dal/PromotionsDAO.java
@@ -17,9 +17,7 @@ public class PromotionsDAO extends CampaignDAO {
      * Đẩy/Gửi dữ liệu đi: Gọi Constructor lớp cha.
      * Action/Luồng đi: Chuyển tiếp context lên lớp cha.
      */
-    public PromotionsDAO(ServletContext context) {
-        super(context);
-    }
+ 
 
     /**
      * Chức năng: Truy vấn danh sách các chiến dịch khuyến mãi có hỗ trợ tìm kiếm theo từ khóa và phân trang dữ liệu trong CSDL.
